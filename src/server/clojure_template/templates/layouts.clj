0bae9b086133e314a49187b89a560b9f44ef22c4
hung-phan <colorvisavn@gmail.com>
2016-07-25 14:39:55 +0700

    Add bootstrap and font awesome dependencies

--- src/server/clojure_template/templates/layouts.clj
+++ src/server/clojure_template/templates/layouts.clj
@@ -10,7 +10,11 @@
           [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
           [:meta {:csrf *anti-forgery-token*}]
           [:title "Application"]
+          (include-css "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css")
+          (include-css "https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css")
           (include-css "css/app.css")
           [:body
            content
+           (include-js "https://code.jquery.com/jquery-2.2.4.min.js")
+           (include-js "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js")
            (include-js "js/compiled/clojure_template.js")]]))
