54e74e4ab44d1fe5ac1ab865c308f9f78f242638
hung-phan <colorvisavn@gmail.com>
2016-10-16 13:59:05 +0700

    Remove csrf token from template

--- src/server/clojure_template/templates/layouts.clj
+++ src/server/clojure_template/templates/layouts.clj
@@ -1,14 +1,12 @@
 (ns clojure-template.templates.layouts
   (:require [environ.core :refer [env]]
-            [hiccup.page :refer [html5 include-css include-js]]
-            [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]))
+            [hiccup.page :refer [html5 include-css include-js]]))
 
 (defn application [& content]
   (html5 [:head
           [:meta {:charset "utf-8"}]
           [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
           [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-          [:meta {:csrf *anti-forgery-token*}]
           [:title "Application"]
           (include-css "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css")
           (include-css "https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css")
