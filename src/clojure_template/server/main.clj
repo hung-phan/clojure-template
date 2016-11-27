14f1c4c81d250307bc5e8a39670e89bb135a64d4
hung-phan <colorvisavn@gmail.com>
2016-11-27 17:44:32 +0000

    Update routing

--- src/clojure_template/server/main.clj
+++ src/clojure_template/server/main.clj
@@ -1,27 +1,12 @@
 (ns clojure-template.server.main
-  (:require [compojure.core :refer [ANY GET PUT POST DELETE defroutes]]
-            [compojure.route :refer [resources not-found]]
-            [compojure.handler :refer [site]]
-            [ring.util.response :refer [redirect]]
+  (:require [compojure.handler :refer [site]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.gzip :refer [wrap-gzip]]
             [ring.middleware.logger :refer [wrap-with-logger]]
             [environ.core :refer [env]]
-            [bidi.bidi :as bidi]
             [org.httpkit.server :refer [run-server]]
-            [clojure-template.server.controllers.application :as application-controller]
-            [clojure-template.server.apis.base :as base-apis]
-            [clojure-template.common.routes :refer [app-routes]])
+            [clojure-template.server.routing :refer [routes]])
   (:gen-class))
-
-(defroutes routes
-           (resources "/public")
-           base-apis/apis
-           (GET "/*" [& request]
-             ;; handle routing in client side
-             (if (bidi/match-route app-routes (str "/" (:* request)))
-               (application-controller/index)
-               (redirect "/404.html"))))
 
 (def http-handler
   (-> routes
