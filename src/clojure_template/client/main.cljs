58e502670b000f0433232da1d2dd83edc34b9981
hung-phan <colorvisavn@gmail.com>
2017-01-18 22:56:04 +0700

    Remove macro require

--- src/clojure_template/client/main.cljs
+++ src/clojure_template/client/main.cljs
@@ -1,5 +1,4 @@
 (ns clojure-template.client.main
-  (:require-macros [reagent.ratom :refer [reaction]])
   (:require [clojure-template.client.dev]
             [clojure-template.client.libs]
             [clojure-template.client.routing]))
