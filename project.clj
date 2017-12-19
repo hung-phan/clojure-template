eb04baa46db1aa204ce6c058538b8242a0c9ed12
hungphan <colorvisavn@gmail.com>
2017-12-19 11:42:20 +0800

    Reset clojure dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT"
             :url  "https://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.946"]
                  [org.clojure/data.generators "0.1.2"]
                  [org.clojure/core.async "0.3.465"]
